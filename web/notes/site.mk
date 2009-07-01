b5fcc0b0e2eeb1185cf3ecba3f861752f7d4d026
François Pinard <pinard@iro.umontreal.ca>
2009-07-01 15:53:12 -0400

    Web site

---
+++ web/notes/site.mk
@@ -0,0 +1,5 @@
+package_name = xxml
+long_package_name = "xxml Emacs mode"
+rootdir = $(HOME)/entretien/xxml/web/notes
+
+TEMPLATE = $(HOME)/entretien/mes-sites/gabarit.html
---
+++ web/site.mk
@@ -0,0 +1,9 @@
+package_name = xxml
+long_package_name = "xxml Emacs mode"
+rootdir = $(HOME)/entretien/xxml/web
+margin_color = "\#d1b7ff"
+caption_color = "\#d7ebc4"
+
+SITE_ROOT = 1
+
+TEMPLATE = $(HOME)/entretien/mes-sites/gabarit.html
